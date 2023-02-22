5d2eee4944af7fed0002b19bf2cf204cb20d2fd7
Marcel Tricolici <marcel@tricolici.com>
2023-02-22 13:46:44 +0200

    implement training

--- SupervisedLearning/FeedforwardNeuralNetwork/ai/network.go
+++ SupervisedLearning/FeedforwardNeuralNetwork/ai/network.go
@@ -1,5 +1,7 @@
 package ai
 
+import "fmt"
+
 type FeedForwardNeuralNetwork struct {
 	inputLayer  *Layer
 	hiddenLayer *Layer
@@ -22,4 +24,58 @@ func (n *FeedForwardNeuralNetwork) Predict(inputs []float64) []float64 {
 	hiddenOutputs := n.hiddenLayer.Activate(inputs)
 	outputOutputs := n.outputLayer.Activate(hiddenOutputs)
 	return outputOutputs
+}
+
+func (n *FeedForwardNeuralNetwork) Train(inputs [][]float64, targets [][]float64, numEpochs int, learningRate float64) {
+	for epoch := 0; epoch < numEpochs; epoch++ {
+		totalError := 0.0
+		for i := range inputs {
+			// Forward pass
+			hiddenOutputs := n.hiddenLayer.Activate(inputs[i])
+			outputOutputs := n.outputLayer.Activate(hiddenOutputs)
+
+			// Calculate output layer errors and deltas
+			outputErrors := make([]float64, len(outputOutputs))
+			for j, output := range outputOutputs {
+				outputErrors[j] = targets[i][j] - output
+				totalError += outputErrors[j] * outputErrors[j]
+			}
+			outputDeltas := make([]float64, len(outputOutputs))
+			for j, output := range outputOutputs {
+				outputDeltas[j] = outputErrors[j] * output * (1 - output)
+			}
+
+			// Calculate hidden layer errors and deltas
+			hiddenErrors := make([]float64, len(hiddenOutputs))
+			for j := range hiddenOutputs {
+				errorSum := 0.0
+				for k, delta := range outputDeltas {
+					errorSum += delta * n.outputLayer.neurons[k].weights[j]
+				}
+				hiddenErrors[j] = errorSum
+			}
+			hiddenDeltas := make([]float64, len(hiddenOutputs))
+			for j, hiddenOutput := range hiddenOutputs {
+				hiddenDeltas[j] = hiddenErrors[j] * hiddenOutput * (1 - hiddenOutput)
+			}
+
+			// Update output layer weights and bias
+			for j, outputNeuron := range n.outputLayer.neurons {
+				for k := range outputNeuron.weights {
+					outputNeuron.weights[k] += learningRate * outputDeltas[j] * hiddenOutputs[k]
+				}
+				outputNeuron.bias += learningRate * outputDeltas[j]
+			}
+
+			// Update hidden layer weights and bias
+			for j, hiddenNeuron := range n.hiddenLayer.neurons {
+				for k := range hiddenNeuron.weights {
+					hiddenNeuron.weights[k] += learningRate * hiddenDeltas[j] * inputs[i][k]
+				}
+				hiddenNeuron.bias += learningRate * hiddenDeltas[j]
+			}
+		}
+		avgError := totalError / float64(len(inputs))
+		fmt.Printf("Epoch %d, Average Error: %f\n", epoch+1, avgError)
+	}
 }
--- SupervisedLearning/FeedforwardNeuralNetwork/example.go
+++ SupervisedLearning/FeedforwardNeuralNetwork/example.go
@@ -4,55 +4,43 @@ import (
 	"MyFeedforwardNeuralNetwork/ai"
 	"MyFeedforwardNeuralNetwork/utils"
 	"fmt"
-	"math/rand"
+	"log"
 	"os"
 	"path/filepath"
 	"strings"
-	"time"
 )
 
 const (
-	inputNeurons       = 3
-	hiddenLayerNeurons = 51
+	inputNeurons       = utils.Size * utils.Size
+	hiddenLayerNeurons = 300
 	outputNeuronsCount = 2 // 1 for cat, 2nd for dog
-	trainingIterations = 100
+	trainingIterations = 10
 	learningRate       = 0.1
 )
 
 func main() {
-	fmt.Println("Hi world")
 
-	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+	home, _ := os.UserHomeDir()
+	trainImagesDirectory := home + "/ai-datasets/dogs-cats/train-normal"
+	fmt.Println("Loading training data into memory ...")
 
-	sample_imput := make([]float64, inputNeurons)
-	for i := range sample_imput {
-		sample_imput[i] = r.Float64()*2 - 1 // random value in [-1 .. 1] range
+	images, labels, err := LoadImages(trainImagesDirectory)
+
+	if err != nil {
+		log.Fatal(err)
 	}
 
+	fmt.Println("Creating a Neural network")
+
+	// Create a neural network
 	nn := ai.NewFeedForwardNeuralNetwork(inputNeurons, hiddenLayerNeurons, outputNeuronsCount)
-	result := nn.Predict(sample_imput)
-	fmt.Println(result)
+	// result := nn.Predict(sample_imput)
 
-	// home, _ := os.UserHomeDir()
-	// trainImagesDirectory := home + "/ai-datasets/dogs-cats/train-normal"
+	fmt.Println("Neural network training started ...")
 
-	// images, labels, err := LoadImages(trainImagesDirectory)
+	nn.Train(images, labels, trainingIterations, learningRate)
 
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-
-	// // Create a neural network with 100 input neurons, 10 hidden neurons, and 4 output neurons
-	// nn := neuralnetwork.NewNeuralNet(utils.Size*utils.Size, outputNeuronsCount, hiddenLayersCount, hiddenLayerNeurons)
-	// nn.RandomizeWeights()
-
-	// // Train the neural network using the images and labels
-	// nn.Train(images, labels, learningRate, trainingIterations)
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-
-	// fmt.Println("Training complete!")
+	fmt.Println("Training complete!")
 	//TODO: load test images and test the NN with new examples
 }
 
