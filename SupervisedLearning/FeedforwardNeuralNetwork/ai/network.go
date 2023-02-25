26137fc2a24ef103be2a75eb3d69f8e6acefe298
Marcel Tricolici <marcel@tricolici.com>
2023-02-25 20:36:32 +0200

    refactoring

--- SupervisedLearning/FeedforwardNeuralNetwork/ai/network.go
+++ SupervisedLearning/FeedforwardNeuralNetwork/ai/network.go
@@ -50,50 +50,64 @@ func (n *FeedForwardNeuralNetwork) train_epoch(
 
 	totalError := 0.0
 	for i := range inputs {
-		// Forward pass
-		hiddenOutputs := n.hiddenLayer.Activate(inputs[i])
-		outputOutputs := n.outputLayer.Activate(hiddenOutputs)
-
-		// Calculate output layer errors and deltas
-		outputErrors := make([]float64, len(outputOutputs))
-		for j, output := range outputOutputs {
-			outputErrors[j] = targets[i][j] - output
-			totalError += outputErrors[j] * outputErrors[j]
-		}
-		outputDeltas := make([]float64, len(outputOutputs))
-		for j, output := range outputOutputs {
-			outputDeltas[j] = outputErrors[j] * output * (1 - output)
-		}
-
-		// Calculate hidden layer errors and deltas
-		hiddenErrors := make([]float64, len(hiddenOutputs))
-		for j := range hiddenOutputs {
-			errorSum := 0.0
-			for k, delta := range outputDeltas {
-				errorSum += delta * n.outputLayer.neurons[k].weights[j]
-			}
-			hiddenErrors[j] = errorSum
-		}
-		hiddenDeltas := make([]float64, len(hiddenOutputs))
-		for j, hiddenOutput := range hiddenOutputs {
-			hiddenDeltas[j] = hiddenErrors[j] * hiddenOutput * (1 - hiddenOutput)
-		}
-
-		// Update output layer weights and bias
-		for j, outputNeuron := range n.outputLayer.neurons {
-			for k := range outputNeuron.weights {
-				outputNeuron.weights[k] += learningRate * outputDeltas[j] * hiddenOutputs[k]
-			}
-			outputNeuron.bias += learningRate * outputDeltas[j]
-		}
-
-		// Update hidden layer weights and bias
-		for j, hiddenNeuron := range n.hiddenLayer.neurons {
-			for k := range hiddenNeuron.weights {
-				hiddenNeuron.weights[k] += learningRate * hiddenDeltas[j] * inputs[i][k]
-			}
-			hiddenNeuron.bias += learningRate * hiddenDeltas[j]
-		}
+		totalError += n.train_input(inputs[i], targets[i], learningRate)
 	}
+
 	return totalError / float64(len(inputs))
+}
+
+func (n *FeedForwardNeuralNetwork) train_input(
+	input []float64, target []float64,
+	learningRate float64) float64 {
+
+	totalError := 0.0
+
+	// Forward pass
+	hiddenOutputs := n.hiddenLayer.Activate(input)
+	outputOutputs := n.outputLayer.Activate(hiddenOutputs)
+
+	// Calculate output layer errors and deltas
+	outputErrors := make([]float64, len(outputOutputs))
+	for j, output := range outputOutputs {
+		outputErrors[j] = target[j] - output
+		totalError += outputErrors[j] * outputErrors[j]
+	}
+
+	outputDeltas := make([]float64, len(outputOutputs))
+	for j, output := range outputOutputs {
+		outputDeltas[j] = outputErrors[j] * output * (1 - output)
+	}
+
+	// Calculate hidden layer errors and deltas
+	hiddenErrors := make([]float64, len(hiddenOutputs))
+	for j := range hiddenOutputs {
+		errorSum := 0.0
+		for k, delta := range outputDeltas {
+			errorSum += delta * n.outputLayer.neurons[k].weights[j]
+		}
+		hiddenErrors[j] = errorSum
+	}
+
+	hiddenDeltas := make([]float64, len(hiddenOutputs))
+	for j, hiddenOutput := range hiddenOutputs {
+		hiddenDeltas[j] = hiddenErrors[j] * hiddenOutput * (1 - hiddenOutput)
+	}
+
+	// Update output layer weights and bias
+	for j, outputNeuron := range n.outputLayer.neurons {
+		for k := range outputNeuron.weights {
+			outputNeuron.weights[k] += learningRate * outputDeltas[j] * hiddenOutputs[k]
+		}
+		outputNeuron.bias += learningRate * outputDeltas[j]
+	}
+
+	// Update hidden layer weights and bias
+	for j, hiddenNeuron := range n.hiddenLayer.neurons {
+		for k := range hiddenNeuron.weights {
+			hiddenNeuron.weights[k] += learningRate * hiddenDeltas[j] * input[k]
+		}
+		hiddenNeuron.bias += learningRate * hiddenDeltas[j]
+	}
+
+	return totalError
 }
