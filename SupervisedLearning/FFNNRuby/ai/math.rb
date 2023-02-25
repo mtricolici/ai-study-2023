72e40baa2e5b55b458fad12bd50c08623cf94a2f
Marcel Tricolici <marcel@tricolici.com>
2023-02-25 11:47:43 +0200

    backpropagation impl in progress

--- SupervisedLearning/FFNNRuby/ai/math.rb
+++ SupervisedLearning/FFNNRuby/ai/math.rb
@@ -6,3 +6,24 @@ end
 def sigmoidDerivative(x)
     x * (1 - x)
 end
+
+def array_sum_elements(arr)
+    sum = 0
+    arr.each do |num|
+      sum += num
+    end
+    return sum
+end
+
+def array_minus_array(arr1, arr2)
+    if arr1.size != arr2.size
+        raise "array_minus_array: Arrays must be of the same size!"
+    end
+
+    result = []
+    arr1.each_with_index do |val, i|
+        result[i] = val + arr2[i]
+    end
+
+    return result
+end
--- SupervisedLearning/FFNNRuby/ai/network.rb
+++ SupervisedLearning/FFNNRuby/ai/network.rb
@@ -11,21 +11,43 @@ class Network
         @layer2.activate(outputs)
     end
 
+    # Backpropagation training
     def train(inputs:, labels:, num_epochs:, learning_rate:)
         puts "Backpropagation training started"
 
         num_epochs.times do |epoch|
-            avgError = train_iteration(
+            error = train_epoch(
                 inputs: inputs, labels: labels, learning_rate: learning_rate)
             
-            puts "Epoch #{epoch + 1}: Average error = #{avgError}"
+            puts "Epoch #{epoch + 1}, Error: #{error}"
         end
     end
 
     private
 
-    def train_iteration(inputs:, labels:, learning_rate:)
-        return 0.666
+    def train_epoch(inputs:, labels:, learning_rate:)
+        sum_errors = 0.0
+
+        inputs.each_with_index do |inp, i|
+            sum_errors += train_one_input(
+                input: inp, label: labels[i], learning_rate: learning_rate)
+        end
+
+        return sum_errors # / inputs.length()
     end
 
+    def train_one_input(input:, label:, learning_rate:)
+        # compute specific input
+        layer1_output = @layer1.activate(input)
+        layer2_output = @layer2.activate(layer1_output)
+
+        # Calculate error: difference between expected-output (i.e. label) and actual-output (i.e. layer2_output)
+        errors = array_minus_array(label, layer2_output)
+        sumErrors = array_sum_elements(errors)
+
+        # Backward pass
+
+
+        return sumErrors
+    end
 end
