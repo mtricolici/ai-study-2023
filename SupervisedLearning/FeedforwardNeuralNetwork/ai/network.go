64c9916703c5957e163e51736269d5b5ea3d4a06
Marcel Tricolici <marcel@tricolici.com>
2023-02-25 21:53:45 +0200

    refactoring

--- SupervisedLearning/FeedforwardNeuralNetwork/ai/network.go
+++ SupervisedLearning/FeedforwardNeuralNetwork/ai/network.go
@@ -2,27 +2,28 @@ package ai
 
 import (
 	"log"
+	"math"
 )
 
 type FeedForwardNeuralNetwork struct {
-	hiddenLayer *Layer
-	outputLayer *Layer
+	layer1 *Layer
+	layer2 *Layer
 }
 
 func NewFeedForwardNeuralNetwork(numInput, numHidden, numOutput int) *FeedForwardNeuralNetwork {
-	hiddenLayer := NewLayer(numHidden, numInput, ActivationSigmoid)
-	outputLayer := NewLayer(numOutput, numHidden, ActivationSigmoid)
+	layer1 := NewLayer(numHidden, numInput, ActivationSigmoid)
+	layer2 := NewLayer(numOutput, numHidden, ActivationSigmoid)
 
 	return &FeedForwardNeuralNetwork{
-		hiddenLayer: hiddenLayer,
-		outputLayer: outputLayer,
+		layer1: layer1,
+		layer2: layer2,
 	}
 }
 
 func (n *FeedForwardNeuralNetwork) Predict(inputs []float64) []float64 {
-	hiddenOutputs := n.hiddenLayer.Activate(inputs)
-	outputOutputs := n.outputLayer.Activate(hiddenOutputs)
-	return outputOutputs
+	layer1_outputs := n.layer1.Activate(inputs)
+	layer2_outputs := n.layer2.Activate(layer1_outputs)
+	return layer2_outputs
 }
 
 func (n *FeedForwardNeuralNetwork) Train(
@@ -63,50 +64,50 @@ func (n *FeedForwardNeuralNetwork) train_input(
 	totalError := 0.0
 
 	// Forward pass
-	hiddenOutputs := n.hiddenLayer.Activate(input)
-	outputOutputs := n.outputLayer.Activate(hiddenOutputs)
+	layer1_outputs := n.layer1.Activate(input)
+	layer2_outputs := n.layer2.Activate(layer1_outputs)
 
 	// Calculate output layer errors and deltas
-	outputErrors := make([]float64, len(outputOutputs))
-	for j, output := range outputOutputs {
-		outputErrors[j] = target[j] - output
-		totalError += outputErrors[j] * outputErrors[j]
+	layer2_errors := make([]float64, len(layer2_outputs))
+	for j, output := range layer2_outputs {
+		layer2_errors[j] = target[j] - output
+		totalError += math.Abs(layer2_errors[j]) //* outputErrors[j]
 	}
 
-	outputDeltas := make([]float64, len(outputOutputs))
-	for j, output := range outputOutputs {
-		outputDeltas[j] = outputErrors[j] * output * (1 - output)
+	layer2_delta := make([]float64, len(layer2_outputs))
+	for j, output := range layer2_outputs {
+		layer2_delta[j] = layer2_errors[j] * output * (1 - output)
 	}
 
 	// Calculate hidden layer errors and deltas
-	hiddenErrors := make([]float64, len(hiddenOutputs))
-	for j := range hiddenOutputs {
+	layer1_errors := make([]float64, len(layer1_outputs))
+	for j := range layer1_outputs {
 		errorSum := 0.0
-		for k, delta := range outputDeltas {
-			errorSum += delta * n.outputLayer.neurons[k].weights[j]
+		for k, delta := range layer2_delta {
+			errorSum += delta * n.layer2.neurons[k].weights[j]
 		}
-		hiddenErrors[j] = errorSum
+		layer1_errors[j] = errorSum
 	}
 
-	hiddenDeltas := make([]float64, len(hiddenOutputs))
-	for j, hiddenOutput := range hiddenOutputs {
-		hiddenDeltas[j] = hiddenErrors[j] * hiddenOutput * (1 - hiddenOutput)
+	layer1_delta := make([]float64, len(layer1_outputs))
+	for j, hiddenOutput := range layer1_outputs {
+		layer1_delta[j] = layer1_errors[j] * hiddenOutput * (1 - hiddenOutput)
 	}
 
 	// Update output layer weights and bias
-	for j, outputNeuron := range n.outputLayer.neurons {
-		for k := range outputNeuron.weights {
-			outputNeuron.weights[k] += learningRate * outputDeltas[j] * hiddenOutputs[k]
+	for j, neuron := range n.layer2.neurons {
+		for k := range neuron.weights {
+			neuron.weights[k] += learningRate * layer2_delta[j] * layer1_outputs[k]
 		}
-		outputNeuron.bias += learningRate * outputDeltas[j]
+		neuron.bias += learningRate * layer2_delta[j]
 	}
 
 	// Update hidden layer weights and bias
-	for j, hiddenNeuron := range n.hiddenLayer.neurons {
-		for k := range hiddenNeuron.weights {
-			hiddenNeuron.weights[k] += learningRate * hiddenDeltas[j] * input[k]
+	for j, neuron := range n.layer1.neurons {
+		for k := range neuron.weights {
+			neuron.weights[k] += learningRate * layer1_delta[j] * input[k]
 		}
-		hiddenNeuron.bias += learningRate * hiddenDeltas[j]
+		neuron.bias += learningRate * layer1_delta[j]
 	}
 
 	return totalError
