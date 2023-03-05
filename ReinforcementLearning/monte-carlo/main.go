53dbfcf29348b71c00c199012dd599c1df17c86f
Marcel Tricolici <marcel@tricolici.com>
2023-03-05 11:13:31 +0200

    train more games

--- ReinforcementLearning/monte-carlo/main.go
+++ ReinforcementLearning/monte-carlo/main.go
@@ -69,7 +69,7 @@ func main() {
 
 	switch args[0] {
 	case "train":
-		invoke_MonteCarlo(1_000_000, "/tmp/brain.zzz")
+		invoke_MonteCarlo(5_000_000, "/tmp/brain.zzz")
 	case "demo":
 		play_DemoGame("/tmp/brain.zzz")
 	default:
