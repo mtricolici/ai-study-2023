7c28499b6a538196a13a139aed38dab7f4dba971
Marcel Tricolici <marcel@tricolici.com>
2023-03-09 21:57:39 +0200

    run in docker. gpu and tensorflow setup is a hell

---
+++ ReinforcementLearning/deep-q-learning/python_sample/run-in-docker.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e
+
+repo_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")/../../.." >/dev/null 2>&1 && pwd)"
+
+docker run --gpus all -it \
+  -v $repo_dir:/zzz:ro \
+  -w /zzz/ReinforcementLearning/deep-q-learning/python_sample/ \
+  --rm \
+  -u $(id -u):$(id -g) \
+  tensorflow/tensorflow:latest-gpu \
+  ./main.py $@
