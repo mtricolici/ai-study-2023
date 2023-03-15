9e9e74dae6fd78178460e956e26b8053cd11da49
Marcel Tricolici <marcel@tricolici.com>
2023-03-15 09:34:15 +0200

    small fix in run.sh

--- ReinforcementLearning/genetic-algorithm/golang_snake_ga/run.sh
+++ ReinforcementLearning/genetic-algorithm/golang_snake_ga/run.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
+if [ ! -f /tmp/libsnake.so ]; then
+  echo "File /tmp/libsnake.so not found!"
+  echo "cd to ../../../c-ui-app/ and invoke ./compile.sh !"
+  exit 1
+fi
+
 go run -exec "env LD_LIBRARY_PATH=/tmp" main.go "$@"
