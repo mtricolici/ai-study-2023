9c1f20880108f50e39196cd455e9f132a4598c07
Marcel Tricolici <marcel@tricolici.com>
2023-11-19 16:53:54 +0200

    bug fix

--- pylibs/snake/run-in-docker.sh
+++ pylibs/snake/run-in-docker.sh
@@ -1,9 +1,27 @@
 #!/bin/bash
 set -e # exit on error
 
-docker build \
-  -t pylib_snake \
-  .
+IMG=my-pylib-snake
+
+# seconds since Dockerfile was modified
+DOCKERFILE_MODIFIED=$(stat -c %Y Dockerfile)
+
+# check if image exists
+if [ -z "$(docker images -q $IMG 2>/dev/null)" ]; then
+  IMAGE_CREATED=0
+else
+  IMAGE_CREATED=$(docker inspect -f '{{.Created}}' $IMG)
+  IMAGE_CREATED=$(date -d "$IMAGE_CREATED" +%s)
+fi
+
+
+if [ "$DOCKERFILE_MODIFIED" -gt "$IMAGE_CREATED" ]; then
+  docker build \
+    --no-cache \
+    --build-arg USER_ID=$(id -u) \
+    --build-arg GROUP_ID=$(id -g) \
+    -t $IMG .
+fi
 
 docker run -it --rm \
   -e DISPLAY="$DISPLAY" \
@@ -11,5 +29,5 @@ docker run -it --rm \
   --network host \
   -v /tmp/.X11-unix:/tmp/.X11-unix \
   -v $(pwd):/app \
-  pylib_snake \
+  $IMG \
   /app/main.py
