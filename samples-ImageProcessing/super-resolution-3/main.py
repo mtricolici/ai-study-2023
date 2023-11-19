6781fb78158ccdcd82713a32fb29c8ee5ae58dfc
Marcel Tricolici <marcel@tricolici.com>
2023-11-19 21:13:17 +0200

    another super-resolution example

---
+++ samples-ImageProcessing/super-resolution-3/main.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env/python
+import argparse
+from ISR.models import RDN
+from PIL import Image
+import numpy as np
+
+def upscale_image(input_path, output_path):
+    # Load the model
+    #model = RDN(weights='psnr-small')
+    model = RDN(weights='psnr-large')
+    #model = RDN(weights='noise-cancel')
+    #model = RRDN(weights='gans')
+
+    # Load the image
+    img = Image.open(input_path)
+
+    # Upscale the image
+    sr_img = model.predict(np.array(img))
+    sr_img = Image.fromarray(sr_img)
+
+    # Save the upscaled image
+    sr_img.save(output_path)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Upscale images using ISR")
+    parser.add_argument("input_path", help="Path to the input image")
+    parser.add_argument("output_path", help="Path to save the output image")
+
+    args = parser.parse_args()
+
+    upscale_image(args.input_path, args.output_path)
+
---
+++ samples-ImageProcessing/super-resolution-3/run.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+set -e
+
+IMG=my-super-image3
+
+mkdir -p .cache
+
+# seconds since Dockerfile was modified
+DOCKERFILE_MODIFIED=$(stat -c %Y Dockerfile)
+
+# check if image exists
+if [ -z "$(docker images -q $IMG 2>/dev/null)" ]; then
+  IMAGE_CREATED=0
+else
+  IMAGE_CREATED=$(docker inspect -f '{{.Created}}' $IMG)
+  IMAGE_CREATED=$(date -d "$IMAGE_CREATED" +%s)
+fi
+
+if [ "$DOCKERFILE_MODIFIED" -gt "$IMAGE_CREATED" ]; then
+  docker build \
+    --no-cache \
+    --build-arg USER_ID=$(id -u) \
+    --build-arg GROUP_ID=$(id -g) \
+    -t $IMG .
+fi
+
+docker run -it --rm \
+  -v $(pwd):/app \
+  -v $(pwd)/.cache:/home/python/.keras \
+  --workdir /tmp \
+  $IMG \
+  python /app/main.py \
+    /app/images/source.png \
+    /app/images/output.png \
+    || echo "Error: $?"
+
