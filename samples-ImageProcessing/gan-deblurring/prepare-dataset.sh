7c2c42d220ab32eedb2425356560b07811dc32bb
Marcel Tricolici <marcel@tricolici.com>
2023-11-22 21:46:51 +0200

    script to prepare dataset for GAN training

---
+++ samples-ImageProcessing/gan-deblurring/prepare-dataset.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+set -e
+
+# Path with images of different sizes
+RAW_DIR=$HOME/temp/raw-images
+
+# output folder to save dataset ready for GAN
+DATASET_DIR=$HOME/temp/dataset
+
+resolutions=("176x144" "256x192" "320x180" "320x240" "420x240" ) #"640x360" "854x480")
+
+RES="?"
+
+# ImageMagic convert filters: "lanczos", "bilinear", "nearestneighbor", "box", "cubic", "mitchell"
+OPS="-filter lanczos -filter mitchell -background black -gravity center -strip"
+
+####################################################
+function select_random_resolution() {
+  local ri=$((RANDOM % ${#resolutions[@]}))
+  local el="${resolutions[$ri]}"
+  local parts=(${el//x/ })
+  local w=${parts[0]}
+  local h=${parts[1]}
+
+  RES="${w}x${h}"
+}
+####################################################
+
+count=1
+total=$(find $RAW_DIR/ -type f |wc -l)
+
+find $RAW_DIR/ -type f -print0 |\
+while IFS= read -d '' img; do
+  select_random_resolution
+
+  oname="$(printf '%05d' $count).png"
+
+  # create a good unblured dataset example
+  echo "Creating $DATASET_DIR/big-$oname"
+  convert "$img" -resize $RES $OPS -extent $RES "$DATASET_DIR/good-$oname"
+
+  echo "Creating $DATASET_DIR/bad-$oname"
+  # scaling down good image to 25%
+  convert "$DATASET_DIR/good-$oname" -resize 25% $OPS "$DATASET_DIR/bad-$oname"
+  # scale up bad image to 400% - as a blurred example
+  convert "$DATASET_DIR/bad-$oname" -resize 400% $OPS "$DATASET_DIR/bad-$oname"
+
+  echo "${count} of ${total} done"
+  
+  ((count++))
+done
+
+echo "Done. Dataset is ready !"
+
