251a43d41d136270a614bdf2aafc080e9b3c2825
Marcel Tricolici <marcel@tricolici.com>
2023-11-28 17:02:30 +0200

    small refactoring in simple CNN

--- samples-ImageProcessing/image-deblurring-attempts/cnn-deblurring/src/models/simple.py
+++ samples-ImageProcessing/image-deblurring-attempts/cnn-deblurring/src/models/simple.py
@@ -1,21 +1,16 @@
-import numpy as np
 import tensorflow as tf
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Conv2D, Conv2DTranspose, BatchNormalization, Activation
-from tensorflow.keras.optimizers import Adam
-from tensorflow.keras.callbacks import ModelCheckpoint, EarlyStopping
-
+from tensorflow.keras import layers, models, optimizers
 from constants import *
 from helper import psnr_metric
 
 #########################################################
 def model_create():
-  model = Sequential([
-    Conv2D(64, (3, 3), padding='same', input_shape=(*INPUT_SIZE[::-1], 3)),
-    Activation('relu'),
-    Conv2D(32, (3, 3), padding='same'),
-    Activation('relu'),
-    Conv2DTranspose(3, (3, 3), padding='same')
+  model = models.Sequential([
+    layers.Conv2D(64, (5, 5), padding='same', input_shape=(*INPUT_SIZE[::-1], 3)),
+    layers.LeakyReLU(alpha=0.01),
+    layers.Conv2D(32, (3, 3), padding='same'),
+    layers.LeakyReLU(alpha=0.01),
+    layers.Conv2DTranspose(3, (3, 3), padding='same')
   ])
   model.compile(
     optimizer=optimizers.Adam(learning_rate=LEARNING_RATE),
