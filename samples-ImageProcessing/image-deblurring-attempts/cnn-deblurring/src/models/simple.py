e6d973098186f77293dec155bbc0ecefec6ea9f2
Marcel Tricolici <marcel@tricolici.com>
2023-11-28 17:06:47 +0200

    try more hidden filters

--- samples-ImageProcessing/image-deblurring-attempts/cnn-deblurring/src/models/simple.py
+++ samples-ImageProcessing/image-deblurring-attempts/cnn-deblurring/src/models/simple.py
@@ -8,7 +8,7 @@ def model_create():
   model = models.Sequential([
     layers.Conv2D(64, (5, 5), padding='same', input_shape=(*INPUT_SIZE[::-1], 3)),
     layers.LeakyReLU(alpha=0.01),
-    layers.Conv2D(32, (3, 3), padding='same'),
+    layers.Conv2D(64, (3, 3), padding='same'),
     layers.LeakyReLU(alpha=0.01),
     layers.Conv2DTranspose(3, (3, 3), padding='same')
   ])
