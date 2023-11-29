09310e7303d306e233871ad82e738b926eb3beea
Marcel Tricolici <marcel@tricolici.com>
2023-11-29 19:51:05 +0200

    more fixes

--- samples-ImageProcessing/ml/colorization/cnn/src/model.py
+++ samples-ImageProcessing/ml/colorization/cnn/src/model.py
@@ -1,5 +1,5 @@
 import tensorflow as tf
-from tensorflow.keras import layers, models, optimizers
+from tensorflow.keras import layers, models, optimizers, initializers
 from constants import *
 from helper import psnr_metric
 
@@ -8,19 +8,19 @@ def model_create():
   input_layer = layers.Input(shape=(None, None, 1))
 
   # Encoding layers
-  x = layers.Conv2D(64, (3, 3), activation='relu', padding='same')(input_layer)
-  x = layers.Conv2D(128, (3, 3), activation='relu', padding='same', strides=2)(x)
-  x = layers.Conv2D(256, (3, 3), activation='relu', padding='same', strides=2)(x)
+  x = layers.Conv2D(64, (3, 3), activation='relu', padding='same', kernel_initializer=initializers.HeNormal())(input_layer)
+  x = layers.Conv2D(128, (3, 3), activation='relu', padding='same', kernel_initializer=initializers.HeNormal(), strides=2)(x)
+  x = layers.Conv2D(256, (3, 3), activation='relu', padding='same', strides=2, kernel_initializer=initializers.HeNormal())(x)
 
   # Decoding layers with UpSampling
-  x = layers.Conv2D(256, (3, 3), activation='relu', padding='same')(x)
+  x = layers.Conv2D(256, (3, 3), activation='relu', padding='same', kernel_initializer=initializers.HeNormal())(x)
   x = layers.UpSampling2D((2, 2))(x)
-  x = layers.Conv2D(128, (3, 3), activation='relu', padding='same')(x)
+  x = layers.Conv2D(128, (3, 3), activation='relu', padding='same', kernel_initializer=initializers.HeNormal())(x)
   x = layers.UpSampling2D((2, 2))(x)
-  x = layers.Conv2D(64, (3, 3), activation='relu', padding='same')(x)
+  x = layers.Conv2D(64, (3, 3), activation='relu', padding='same', kernel_initializer=initializers.HeNormal())(x)
 
   # Output layer with 3 channels (RGB color)
-  output_layer = layers.Conv2D(3, (3, 3), activation='sigmoid', padding='same')(x)
+  output_layer = layers.Conv2D(3, (3, 3), activation='sigmoid', padding='same', kernel_initializer=initializers.HeNormal())(x)
 
   model = models.Model(inputs=input_layer, outputs=output_layer)
 
