38403e0d3f8a05888c3ae66c668b2416f598ea0d
Marcel Tricolici <marcel@tricolici.com>
2023-12-06 23:27:52 +0200

    testing more pre-trained models

--- samples-ImageProcessing/ml/image-restoration/NAFNet/image_test.sh
+++ samples-ImageProcessing/ml/image-restoration/NAFNet/image_test.sh
@@ -2,17 +2,27 @@
 
 IMG=my-nafnet-img-rest
 
-docker run \
-  --gpus all \
-  -it --rm \
-  --network=none \
-  -w /app \
-  -v $(pwd)/src:/app:ro \
-  -v $(pwd)/.images:/images \
-  -v $(pwd)/models:/models:ro \
-  $IMG \
-  python demo.py \
-  -opt /app/NAFNet-width64.yml \
-  --input_path /images/src.png \
-  --output_path /images/result.png
+#model="NAFNet-REDS-width64" # <<< deblurr but very aggresive - unreal images :)
+#model="NAFNet-SIDD-width64" # << noise removal
+#model="NAFNet-GoPro-width64" # << deblur but not very aggresive :)
+
+models=("NAFNet-REDS-width64" "NAFNet-SIDD-width64" "NAFNet-GoPro-width64")
+
+for model in "${models[@]}"; do
+  echo "Using model '$model'"
+  docker run \
+    --gpus all \
+    -it --rm \
+    --network=none \
+    -w /app \
+    -v $(pwd)/src:/app:ro \
+    -v $(pwd)/.images:/images \
+    -v $(pwd)/models:/models:ro \
+    $IMG \
+    python demo.py \
+    -opt /app/$model.yml \
+    --input_path /images/src.png \
+    --output_path /images/result-${model}.png
+done
+
 
