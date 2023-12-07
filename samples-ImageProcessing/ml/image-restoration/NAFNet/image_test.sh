11f8054c6ce2608891c586c06ea8d1ec4781ce71
Marcel Tricolici <marcel@tricolici.com>
2023-12-07 16:36:31 +0200

    test more models

--- samples-ImageProcessing/ml/image-restoration/NAFNet/image_test.sh
+++ samples-ImageProcessing/ml/image-restoration/NAFNet/image_test.sh
@@ -2,11 +2,14 @@
 
 IMG=my-nafnet-img-rest
 
-#model="NAFNet-REDS-width64" # <<< deblurr but very aggresive - unreal images :)
-#model="NAFNet-SIDD-width64" # << noise removal
-#model="NAFNet-GoPro-width64" # << deblur but not very aggresive :)
+#model="NAFNet-REDS-width64"    # <<< deblurr but very aggresive - unreal images :)
+#model="NAFNet-SIDD-width64"    # << noise removal
+#model="NAFNet-GoPro-width64"   # << deblur but not very aggresive :)
+#model="Baseline-GoPro-width64" # << ???
+#model="Baseline-SIDD-width64"  # << ???
 
-models=("NAFNet-REDS-width64" "NAFNet-SIDD-width64" "NAFNet-GoPro-width64")
+#models=("NAFNet-REDS-width64" "NAFNet-SIDD-width64" "NAFNet-GoPro-width64")
+models=("NAFNet-REDS-width64" "Baseline-GoPro-width64" "Baseline-SIDD-width64")
 
 for model in "${models[@]}"; do
   echo "Using model '$model'"
