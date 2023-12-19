7c35d0a6b9c8d71a0578420302d0c12dc93290e2
Marcel Tricolici <marcel@tricolici.com>
2023-12-19 22:42:06 +0200

    code-former seems to be the best face enhancer

--- samples-ImageProcessing/ml/face-swap/facefusion/test_face_enhancer.sh
+++ samples-ImageProcessing/ml/face-swap/facefusion/test_face_enhancer.sh
@@ -11,24 +11,31 @@ fi
 
 mkdir -p content
 
-detect_model="retinaface_10g" # Seems to be the best one
+detect_model="yunet_2023mar" # retinaface_10g or yunet_2023mar
 recognizer_model="arcface_w600k_r50" # arcface_w600k_r50 or arcface_simswap
 
 enh_model="gfpgan_1.4"
 
-docker run \
-  --gpus all \
-  -it --rm \
-  --network=none \
-  -w /app \
-  -v $(pwd)/src:/app:ro \
-  -v $(pwd)/content:/content \
-  -v $(pwd)/models:/models:ro \
-  $IMG \
-  python /app/main.py \
-    face-enh \
-    -i /content/src.png \
-    -o /content/result-enh-faces-${enh_model}.png \
-    --detect-model $detect_model \
-    --rec-model $recognizer_model \
-    --face-enh-model $enh_model
+for enh_model in models/face-enh/*.onnx; do
+  enh_model=${enh_model##*/}
+  enh_model=${enh_model%.onnx}
+  echo "Invoking face-enhancement model '${enh_model}' ..."
+
+  docker run \
+    --gpus all \
+    -it --rm \
+    --network=none \
+    -w /app \
+    -v $(pwd)/src:/app:ro \
+    -v $(pwd)/content:/content \
+    -v $(pwd)/models:/models:ro \
+    $IMG \
+    python /app/main.py \
+      face-enh \
+      -i /content/src.png \
+      -o /content/result-enh-faces-${enh_model}.png \
+      --detect-model $detect_model \
+      --rec-model $recognizer_model \
+      --face-enh-model $enh_model
+done
+
