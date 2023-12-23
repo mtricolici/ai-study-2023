71c56bb864f66874366a5d000020a34ae92c71d8
Marcel Tricolici <marcel@tricolici.com>
2023-12-24 00:53:27 +0200

    code refactoring

--- samples-ImageProcessing/ml/autoencoders/vae/src/main.py
+++ samples-ImageProcessing/ml/autoencoders/vae/src/main.py
@@ -4,7 +4,6 @@ import tensorflow as tf
 import numpy as np
 
 from model import VAE
-from train import train
 from image import save_image
 
 latent_dim = 512
@@ -14,36 +13,31 @@ input_shape = (128, 128, 3)
 def invoke_train():
     print('creating model ...')
     vae = VAE(latent_dim, input_shape)
-    vae.create_model()
     print('starting training ...')
     try:
-      train(vae)
+      vae.train()
     except KeyboardInterrupt:
         print('Aborting ...')
     print('saving model ...')
-    vae.save_model('/content')
+    vae.save_model()
 #########################################################
 def continue_train():
     print('creating model ...')
     vae = VAE(latent_dim, input_shape)
-    vae.create_model()
-    print('Loading weights ...')
-    vae.load_model('/content')
+    vae.load_model()
     print('starting training ...')
     try:
-      train(vae)
+      vae.train()
     except KeyboardInterrupt:
         print('Aborting ...')
     print('saving model ...')
-    vae.save_model('/content')
+    vae.save_model()
 #########################################################
-def invoke_demo(num_samples=20):
+def invoke_demo():
     vae = VAE(latent_dim, input_shape)
-    vae.create_model()
-    vae.load_model('/content')
-    random_latent_points = np.random.normal(size=(num_samples, latent_dim))
-    samples = vae.decoder.predict(random_latent_points)
-    for i in range(num_samples):
+    vae.load_model()
+    samples = vae.generate_samples(20)
+    for i, sample in enumerate(samples):
         filename = f'/content/result-{i + 1}.png'
         save_image(samples[i], filename)
 #########################################################
--- samples-ImageProcessing/ml/autoencoders/vae/src/model.py
+++ samples-ImageProcessing/ml/autoencoders/vae/src/model.py
@@ -4,14 +4,60 @@ from tensorflow.keras import layers
 import numpy as np
 
 from constants import *
+import dataset as ds
 
 class VAE:
+#########################################################
     def __init__(self, latent_dim, input_shape):
         self.latent_dim = latent_dim
         self.input_shape = input_shape
         self.encoder = None
         self.decoder = None
+        self.create_model()
+#########################################################
+    def save_model(self):
+        self.encoder.save_weights('/content/model_encoder.h5')
+        self.decoder.save_weights('/content/model_decoder.h5')
 
+#########################################################
+    def load_model(self):
+        self.encoder.load_weights('/content/model_encoder.h5')
+        self.decoder.load_weights('/content/model_decoder.h5')
+#########################################################
+    def generate_samples(self, num_samples):
+        random_latent_points = np.random.normal(size=(num_samples, self.latent_dim))
+        return self.decoder.predict(random_latent_points)
+#########################################################
+    @tf.function(reduce_retracing=True)
+    def _train_step(self, optimizer, x_batch, loss_metric):
+        with tf.GradientTape() as tape:
+            z_mean, z_log_var, z = self.encoder(x_batch)
+            reconstruction = self.decoder(z)
+            reconstruction_loss = tf.reduce_mean(
+                tf.keras.losses.binary_crossentropy(x_batch, reconstruction))
+            kl_loss = -0.5 * tf.reduce_mean(
+                z_log_var - tf.square(z_mean) - tf.exp(z_log_var) + 1)
+            loss = reconstruction_loss + kl_loss
+
+        grads = tape.gradient(loss, self.encoder.trainable_variables + self.decoder.trainable_variables)
+        optimizer.apply_gradients(zip(grads, self.encoder.trainable_variables + self.decoder.trainable_variables))
+
+        loss_metric(loss)
+#########################################################
+    def _train_epoch(self, optimizer, dl, loss_metric):
+        for step in range(STEPS_PER_EPOCH):
+            x_batch = next(dl)
+            self._train_step(optimizer, x_batch, loss_metric)
+#########################################################
+    def train(self):
+        dl = ds.data_loader()
+        optimizer = tf.keras.optimizers.Adam(learning_rate=LEARNING_RATE)
+        loss_metric = tf.keras.metrics.Mean()
+
+        for epoch in range(EPOCH):
+            self._train_epoch(optimizer, dl, loss_metric)
+            print(f"Epoch {epoch + 1}/{EPOCH}, Loss: {loss_metric.result()}")
+#########################################################
     def create_model(self):
         # Encoder model
         inputs = tf.keras.Input(shape=self.input_shape)
@@ -32,17 +78,10 @@ class VAE:
 
         self.decoder = tf.keras.Model(latent_inputs, outputs, name='decoder')
 
+#########################################################
     def reparameterize(self, z_mean, z_log_var):
         eps = tf.random.normal(shape=tf.shape(z_mean))
         return z_mean + tf.exp(0.5 * z_log_var) * eps
 
-    def save_model(self, model_dir):
-        self.encoder.save_weights(model_dir + '/encoder_weights.h5')
-        self.decoder.save_weights(model_dir + '/decoder_weights.h5')
-
-    def load_model(self, model_dir):
-        self.create_model()
-        self.encoder.load_weights(model_dir + '/encoder_weights.h5')
-        self.decoder.load_weights(model_dir + '/decoder_weights.h5')
-
+#########################################################
 
