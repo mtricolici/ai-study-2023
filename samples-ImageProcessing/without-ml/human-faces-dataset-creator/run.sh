00c4673ee13fac8c5f572a2133c6338d1f68e620
Marcel Tricolici <marcel@tricolici.com>
2024-01-01 15:56:46 +0200

    small fix - different paths

--- samples-ImageProcessing/without-ml/human-faces-dataset-creator/run.sh
+++ samples-ImageProcessing/without-ml/human-faces-dataset-creator/run.sh
@@ -3,18 +3,22 @@ set -e
 
 IMG=my-human-faces-ds-creator
 
-mkdir -p .insightface
+mkdir -p .insightface .cache
 
 docker run \
   --gpus all \
   -it --rm \
+  -e TF_FORCE_GPU_ALLOW_GROWTH=true \
+  -e TF_CPP_MIN_LOG_LEVEL=3 \
   -w /app \
   -v $(pwd)/src:/app:ro \
   -v $(pwd)/.insightface:/home/python/.insightface \
-  -v $HOME/temp/raw-images:/raw-images:ro \
-  -v $HOME/temp/human-faces-dataset:/human-faces \
+  -v $(pwd)/.cache:/home/python/.cache \
+  -v $HOME/datasets/raw-images:/raw-images:ro \
+  -v $HOME/datasets/human-faces:/human-faces \
   $IMG \
   python main.py \
     --source-path /raw-images \
     --output-path /human-faces \
     --device cuda
+
